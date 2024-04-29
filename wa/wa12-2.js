49f06e9479b32c26e6f64d604842f0ae6c17c44d
jbpink <japi2364@colorado.edu>
2024-04-29 12:53:59 -0600

    wa12.2

--- wa/wa12-2.js
+++ wa/wa12-2.js
@@ -9,7 +9,6 @@ async function getQuote() {
   const factCount = parseInt(factCountInput.value, 10);
   let endpoint = baseEndpoint;
   
-  // Validate the number of facts requested
   if(factCount < 1 || factCount > 3) {
     alert(`Error: Please enter a number between 1 and 3. You entered: ${factCount}`);
     return;
@@ -25,8 +24,7 @@ async function getQuote() {
       throw Error(response.statusText);
     }
     const json = await response.json();
-    // Assuming the API returns an array of facts when 'count' parameter is used
-    displayQuote(json.data.join('<br><br>')); // Join the facts with a break line
+    displayQuote(json.data.join('<br><br>')); 
   } catch (err) {
     console.error(err);
     alert('Failed to fetch new quote');
@@ -35,7 +33,7 @@ async function getQuote() {
 
 function displayQuote(quote) {
   const quoteText = document.querySelector('#js-quote-text');
-  quoteText.innerHTML = quote; // Using innerHTML to interpret the break lines
+  quoteText.innerHTML = quote; 
 }
 
 getQuote();
